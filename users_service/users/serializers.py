c3e64913b493949d730c4c441dc11ca17d15fab1
xsh <m.hadjsadok@esi-sba.dz>
2025-03-04 19:20:25 +0100

    fix: update UserSerializer to include date_joined field and remove email field

--- users_service/users/serializers.py
+++ users_service/users/serializers.py
@@ -4,8 +4,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
-        read_only_fields = ('id',)
+        fields = ('id', 'username', 'date_joined')
+        read_only_fields = ('id', 'date_joined')
 
     def create(self, validated_data):
         user = User.objects.create_user(
